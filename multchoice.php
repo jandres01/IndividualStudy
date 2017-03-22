9f36bd42cdefe0ab0d47b71fd3d7a7adaef2b123
2013-08-28 <bgarza2@dias11.CS.Trinity.Edu>
2017-03-22 18:30:25 -0500

    finally got the data out from the database

--- multchoice.php
+++ multchoice.php
@@ -2,8 +2,11 @@
 session_start();
 
 if ($_SERVER['SERVER_NAME'] != "dias11.cs.trinity.edu") {
-  header('Location: ./Task_5_part1.php?error=server');
+  echo "get on dias11";
+  die();
 }
+
+print_r($_SESSION);
 
 $servername = "localhost";
 $username = "quizproject";
@@ -15,32 +18,22 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-if (isset($_SESSION["quiz"] && isset($_SESSION["questtype"]) && isset($_SESSION["questnum"])) {
-  echo "The necessary Session variables aren't set for you to be able to use this page, why are you here?";
-  die();
-}
-
-if ($_SESSION["questtype"] == "mc") {
-  $sql = "SELECT * FROM multChoice WHERE quizNum=" . $_SESSION["quiz"] . " LIMIT " . $_SESSION["questnum"] . ", 1;";
-  $result = $conn->query($sql)->fetch_assoc();
-  echo $result["question"];
-  if (!is_null($result["choice1"])) { echo $result["choice1"] };
-  if (!is_null($result["choice2"])) { echo $result["choice2"] };
-  if (!is_null($result["choice3"])) { echo $result["choice3"] };
-  if (!is_null($result["choice4"])) { echo $result["choice4"] };
-  if (!is_null($result["choice5"])) { echo $result["choice5"] };
-  if (!is_null($result["choice6"])) { echo $result["choice6"] };
-} else {
-  $sql = "SELECT * FROM shortAnswer WHERE quizNum=" . $_SESSION["quiz"] . " LIMIT " . $_SESSION["questnum"] . ", 1;";
-  
-}
-
-
-
-$sql = "SELECT * FROM tasks WHERE Username='" . $_SESSION["username"] . "' ORDER BY Date";
-$result = $conn->query($sql);
-echo "Date, Task<br>";
-while($row = $result->fetch_assoc()) {
-  echo $row["Date"] . ": " . $row["Task"] . ' <a href="./Task_5_part3.php?del=true&id=' . $row["ID"] . '">Delete</a><br>';
+if (isset($_SESSION["quiz"]) && isset($_SESSION["questtype"]) && isset($_SESSION["questnum"])) {
+  if ($_SESSION["questtype"] == "mc") {
+    $sql = "SELECT * FROM multChoice WHERE quizNum=" . $_SESSION["quiz"] . " LIMIT " . $_SESSION["questnum"] . ", 1;";
+    $result = $conn->query($sql)->fetch_assoc();
+    echo $result["question"];
+    if (!is_null($result["choice1"])) echo $result["choice1"];
+    if (!is_null($result["choice2"])) echo $result["choice2"];
+    if (!is_null($result["choice3"])) echo $result["choice3"];
+    if (!is_null($result["choice4"])) echo $result["choice4"];
+    if (!is_null($result["choice5"])) echo $result["choice5"];
+    if (!is_null($result["choice6"])) echo $result["choice6"];
+  } else {
+    $sql = "SELECT * FROM shortAnswer WHERE quizNum=" . $_SESSION["quiz"] . " LIMIT " . $_SESSION["questnum"] . ", 1;";
+    $result = $conn->query($sql)->fetch_assoc();
+    echo $result["question"];
+    
+  }
 }
 ?>
--- multchoicePrep.php
+++ multchoicePrep.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
 
+session_unset();
+#session_destroy();
+
 $_SESSION["quiz"] = 1;
-$_SESSION["questtype"] = "mc";
-$_SESSION["questnum"] = 1;
+$_SESSION["questtype"] = "sa";
+$_SESSION["questnum"] = 0;
 
 header('Location: ./multchoice.php');
 ?>
